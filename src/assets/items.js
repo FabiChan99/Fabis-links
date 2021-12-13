38facc2c4be2ca6331b342c29cb06c22577678d1
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2021-12-13 12:33:50 +0100

    Uploaded Local Code

---
+++ src/assets/items.js
@@ -0,0 +1,67 @@
+import instagram from '../assets/instagram.png'
+import youtube from '../assets/youtube.png'
+import github from '../assets/github.webp'
+import twitter from '../assets/twitter.png'
+import twitch from '../assets/twitch.png'
+import discord from '../assets/discord.gif'
+import spotify from '../assets/spotify.png'
+import discordbot from '../assets/discordbot.png'
+
+const items = [
+{
+    "title": "Twitch",
+    "subtitle": "Sei bei Fabi's Streams dabei",
+    "image": twitch,
+    "link": "https://twitch.tv/fabichan99"
+},
+{
+    "title": "Instagram",
+    "subtitle": "Ab und Zu mal bisschen was hier und da",
+    "image": instagram,
+    "link": "https://www.instagram.com/fabi_chan_99/"
+},
+{
+    "title": "YouTube",
+    "subtitle": "Random Videos",
+    "image": youtube,
+    "link": "https://www.youtube.com/channel/UC5FOwFLEWMU_G8VKEZqk6zA"
+},
+{
+    "title": "Mein Community Discord",
+    "subtitle": "Server für meine Streams und mehr",
+    "image": discord,
+    "link": "https://discord.gg/5mMPRrvs55"
+},
+{
+    "title": "Anime & Gaming Café",
+    "subtitle": "Sei dabei mit 13k weiteren Anime & Gaming Fans",
+    "image": discord,
+    "link": "https://discord.gg/animegamingcafe"
+},
+{
+    "title": "Spotify",
+    "subtitle": "Bestimmt für jeden eine Playlist dabei",
+    "image": spotify,
+    "link": "https://open.spotify.com/user/nej9o5jz16mxpwhwizqppderv"
+},
+{
+    "title": "Twitter",
+    "subtitle": "Random Posts idk",
+    "image": twitter,
+    "link": "https://twitter.com/Fabi_Chan99"
+},
+{
+    "title": "GitHub",
+    "subtitle": "Paar sachen wenn mir mal Langweilig ist",
+    "image": github,
+    "link": "https://github.com/FabiChan99"
+},
+{
+    "title": "Hikari Discord Bot",
+    "subtitle": "Mein aktuelles Discord Projekt",
+    "image": discordbot,
+    "link": "https://hikaribot.me"
+}
+]
+
+export default items
